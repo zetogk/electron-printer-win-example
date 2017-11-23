72fbdfc8093f8886e0390270e9fbdcb97e85ae11
Santiago Yepes <zetogk@gmail.com>
2017-11-23 13:29:29 -0500

    Print fromfile in Mac

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 400, height: 400 })
+  mainWindow = new BrowserWindow({ width: 400, height: 700 })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
--- renderer.js
+++ renderer.js
@@ -11,8 +11,10 @@ for (let i = 0; i < printers.length; i++) {
 
 }
 
-btnPrint = document.getElementById('btnPrint');
+btnPrintWin = document.getElementById('btnPrintWin');
+btnPrintMac = document.getElementById('btnPrintMac');
 btnInfo = document.getElementById('btnInfo');
+ulLog = document.getElementById('operationLog');
 
 btnInfo.addEventListener('click', () => {
 
@@ -24,33 +26,96 @@ btnInfo.addEventListener('click', () => {
 });
 
 
-btnPrint.addEventListener('click', function () {
+btnPrintWin.addEventListener('click', function () {
 
   var text = document.getElementById('textareaContent').value;
   var selectedPrinter = document.getElementById('selectPrinter').value;
+  var typePrinting = document.getElementById('selectTypePrinting').value;
 
   if (selectedPrinter === 'select-a-printer') {
 
-    alert('You should select a printer');
+    //alert('You should select a printer');
+    operationLog.append('Your should select a printer \n');
 
   } else {
 
-    alert('The next text will be printed by ' + selectedPrinter);
-    alert(text);
+    if (typePrinting === 'text') {
 
-    printer.printDirect({
-      data: text,
-      printer: selectedPrinter,
-      type: 'RAW',
-      success: function (jobID) {
-        console.log("sent to printer with ID: " + jobID);
-        alert('Printing');
-      },
-      error: function (err) {
-        console.log(err);
-        alert('There was an error to complete the operation');
-      }
-    });
+      printer.printDirect({
+        data: text,
+        printer: selectedPrinter,
+        type: 'RAW',
+        success: function (jobID) {
+          operationLog.append('Printing in '+ selectedPrinter +' \n');
+          /* console.log("sent to printer with ID: " + jobID);
+          alert('Printing'); */
+        },
+        error: function (err) {
+          console.log(err);
+          //alert('There was an error to complete the operation');
+          operationLog.append('There was an error to complete the operation \n');
+        }
+      });
+
+    } else {
+
+      alert('Is not possible print from files to Windows');
+
+    }
+
+  }
+
+});
+
+btnPrintMac.addEventListener('click', function () {
+
+  var text = document.getElementById('textareaContent').value;
+  var typePrinting = document.getElementById('selectTypePrinting').value;
+  var selectedPrinter = document.getElementById('selectPrinter').value;
+  var filePath = document.getElementById('inputFilePath').value;
+
+  if (selectedPrinter === 'select-a-printer') {
+
+    //alert('You should select a printer');
+    operationLog.append('Your should select a printer \n');
+
+  } else {
+
+    if (typePrinting === 'text') {
+
+      printer.printDirect({
+        data: text,
+        printer: selectedPrinter,
+        type: 'RAW',
+        success: function (jobID) {
+          operationLog.append('Printing in '+ selectedPrinter +' \n');
+          /* console.log("sent to printer with ID: " + jobID);
+          alert('Printing'); */
+        },
+        error: function (err) {
+          console.log(err);
+          //alert('There was an error to complete the operation');
+          operationLog.append('There was an error to complete the operation \n');
+        }
+      });
+
+    } else {
+
+      printer.printFile({
+        filename: filePath,
+        printer: selectedPrinter,
+        success: function (jobID) {
+          operationLog.append('Printing in '+ selectedPrinter +' \n');
+          /* console.log("sent to printer with ID: " + jobID);
+          alert('Printing'); */
+        },
+        error: function (err) {
+          //alert('There was an error to complete the operation');
+          operationLog.append('There was an error to complete the operation \n');
+        }
+      });
+
+    }
 
   }
 
